dc9a0ef70ffd62df59a3c5d0c55576790d2d729f
liyunchou <18325613087@163.com>
2024-08-05 10:29:13 +0800

    基础功能

--- src/main/java/com/practice/reggie/filter/LoginCheckFilter.java
+++ src/main/java/com/practice/reggie/filter/LoginCheckFilter.java
@@ -36,7 +36,11 @@ public class LoginCheckFilter implements Filter {
                 "/front/**",
                 "/common/**",
                 "/user/login",
-                "/user/sendMsg"
+                "/user/sendMsg",
+                "/doc.html",
+                "/webjars/**",
+                "/swagger-resources",
+                "/v2/api-docs"
         };
 
         boolean check = check(urls, requestURI);
